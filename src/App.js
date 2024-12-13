30f092c46cafe153454bc118d17274ce562f49ca
Zaina Qamar <zaina.qamar03@gmail.com>
2024-12-12 19:19:41 -0500

    Header created for all screens.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header";
 import Home from "./Home";
 import SellBook from "./SellBook";
 import UserProfile from "./UserProfile";
@@ -9,6 +10,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Router className="App">
+      <Header />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
