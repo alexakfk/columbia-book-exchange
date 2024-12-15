6fd1cce4842f20b0558a210de18c09f2ee086e7d
Amrita Pal <amritapal@Amritas-MacBook-Pro.local>
2024-12-15 13:29:37 -0500

    Updated book content and tags

--- frontend/src/BookCard.js
+++ frontend/src/BookCard.js
@@ -2,25 +2,29 @@ import "./App.css";
 import { MdOutlineEdit } from "react-icons/md";
 import { FaRegTrashAlt } from "react-icons/fa";
 
-function BookCard() {
+function BookCard({ image, altText, title, author, isbn, sellingPrice, originalPrice, condition, description, tags }) {
     return (
         <div className="book-container">
             <img
-                src={require("./images/cover1.png")}
-                alt="Plato's Republic"
+                src={require(`${image}`)}
+                alt={altText}
                 className="cover-image"
             />
             <div className="book-content">
-                <p><strong>Title:</strong> The Republic</p>
-                <p><strong>Author:</strong> Plato</p>
-                <p><strong>ISBN:</strong> 9781503379985</p>
-                <p><strong>Selling Price:</strong> $7.00</p>
-                <p><strong>Original Price:</strong> $16.96</p>
-                <p><strong>Condition:</strong> Lightly Used</p>
-                <p><strong>Description:</strong> I read this book for the fall semester of my CC class.</p>
-                <p><strong>TAG:</strong> WILL INCLUDE TAGS LATER</p>
+                <p><strong>Title:</strong> {title}</p>
+                <p><strong>Author:</strong> {author}</p>
+                <p><strong>ISBN:</strong> {isbn}</p>
+                <p><strong>Selling Price:</strong> ${sellingPrice}</p>
+                <p><strong>Original Price:</strong> ${originalPrice}</p>
+                <p><strong>Condition:</strong> {condition}</p>
+                <p><strong>Description:</strong> {description}</p>
+                <p>{
+                    tags.map((tag, idx) => (
+                        <div className="tag-selected">{tag}</div>
+                    ))
+                }</p>
             </div>
-            <div className="react-icon-bookcard"><FaRegTrashAlt size={21}/>&nbsp;&nbsp;&nbsp;<MdOutlineEdit size={25}/></div>
+            <div className="react-icon-bookcard"><FaRegTrashAlt size={21}/>&nbsp;&nbsp;&nbsp;&nbsp;<MdOutlineEdit size={25}/></div>
         </div>
     );
 }
--- frontend/src/UserProfile.js
+++ frontend/src/UserProfile.js
@@ -6,6 +6,40 @@ import { FaPlus } from "react-icons/fa6";
 import BookCard from "./BookCard";
 
 function UserProfile() {
+  const bookList = [{
+      "image": "./images/cover1.png",
+      "altText": "Plato's Republic", 
+      "title": "The Republic", 
+      "author": "Plato", 
+      "isbn": "9781503379985", 
+      "sellingPrice": "7.00", 
+      "originalPrice": "16.96", 
+      "condition": "Lightly Used", 
+      "description": "I read this book for the fall semester of my CC class.", 
+      "tags": [
+        "CC",
+        "Core",
+        "Humanities",
+        "Paperback"
+      ]
+    }, {
+      "image": "./images/cover2.png",
+      "altText": "Homer's The Iliad", 
+      "title": "The Iliad", 
+      "author": "Homer", 
+      "isbn": "9780226470498", 
+      "sellingPrice": "4.99", 
+      "originalPrice": "10.99", 
+      "condition": "New (no annotations)", 
+      "description": "I read this book for fall semester LitHum.", 
+      "tags": [
+        "LitHum",
+        "Core",
+        "Humanities",
+        "Paperback"
+      ]
+    }]
+
   return (
     <div className="seller-profile-page">
       <div className="page-layout">
@@ -26,10 +60,24 @@ function UserProfile() {
             <h2 className="page-title">Sell a Book</h2>
             <div className="react-icon"><FaPlus size={21}/></div>
           </div>
-          <BookCard />
+          {
+            bookList.map((book, idx) => (
+              <BookCard image={book.image}
+                        altText={book.altText}
+                        title={book.title}
+                        author={book.author}
+                        isbn={book.isbn}
+                        sellingPrice={book.sellingPrice}
+                        originalPrice={book.originalPrice}
+                        condition={book.condition}
+                        description={book.description}
+                        tags={book.tags}/>
+            ))
+          }
+          {/* <BookCard />
           <br />
           <BookCard />
-          <br />
+          <br /> */}
           <div className="form-actions">
             <button type="submit" className="sell-book-button">
               Add a Book to Sell
