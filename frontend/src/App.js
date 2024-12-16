20c5f0bf95ffe8db2e7aa2d5e6eb63185a00c690
Zaina Qamar <zaina.qamar03@gmail.com>
2024-12-15 21:39:22 -0500

    removing purchasing page, updating Readme.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import SellBook from "./SellBook";
-import BookPurchasing from "./BookPurchasing"; 
 import UserProfile from "./UserProfile";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sell-book" element={<SellBook />} />
-        <Route path="/book-purchasing" element={<BookPurchasing />} />
         <Route path="/user-profile" element={<UserProfile />} />
       </Routes>
     </Router>
