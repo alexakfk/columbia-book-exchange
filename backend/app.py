71e57a153d1be4150f95e8715065fc90116b456f
Zaina Qamar <zaina.qamar03@gmail.com>
2024-12-15 21:10:54 -0500

    Functionality for saving books in backend.

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,34 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 
 CORS(app)
 
+books_on_sale = []
+
+with open('books.json','r') as f:
+    data = json.load(f)
+    books_on_sale = data
+
 @app.route('/')
 def home():
     return "Welcome to the Flask Backend!"
 
-# @app.route('/api/data', methods=['GET'])
-# def get_data():
-#     data = {"message": "Hello from Flask API"}
-#     return jsonify(data)
+@app.route('/save_book', methods=['POST'])
+def save_listing():
+    data = request.get_json()
+    if not data:
+        return jsonify({'message': 'No data provided'}), 400
+    books_on_sale.append(data)
+    
+    print(f"Listing saved: {data}")
+    return jsonify({'message': 'Listing saved successfully', 'books_on_sale': books_on_sale}), 200
+
+@app.route('/get_books', methods=['GET'])
+def get_books():
+    return jsonify({'books_on_sale': books_on_sale}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
