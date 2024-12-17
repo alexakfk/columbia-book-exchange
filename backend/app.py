9b7d39f8848dffcfaef63c68947815904ed5f6a8
Amrita Pal <amritapal@Amritas-MacBook-Pro.local>
2024-12-16 20:13:13 -0500

    Added Flask Backend but no working integration yet

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,32 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, 
+     resources={
+         r"/api/get_book_listings": { 
+             "origins": ["http://localhost:3001"] 
+             }
+        }, 
+     supports_credentials=True)
+
+book_listings = []
+
+with open('book_listings.json','r') as f:
+    book_listings = json.load(f)
 
 @app.route('/')
 def home():
     return "Welcome to the Flask Backend!"
+
+@app.route('/api/get_book_listings', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def get_book_listings():
+    response = jsonify(book_listings)
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3001')
+    return response, 200
 
 # @app.route('/api/data', methods=['GET'])
 # def get_data():
